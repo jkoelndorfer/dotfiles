4b052f75790d46ac946b8db3f0aa031a4a9b62b8
John Koelndorfer <jkoelndorfer@gmail.com>
2015-06-06 10:47:02 -0500

    zsh: Use MacVim on OS X.

--- zsh/custom/os/Darwin-post.zsh
+++ zsh/custom/os/Darwin-post.zsh
@@ -2,3 +2,8 @@
 
 # Use GNU coreutils over BSD coreutils.
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH"
+
+# OS X's builtin version of vim has a tendency to crash, so let's use MacVim
+# instead.
+alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
