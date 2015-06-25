3651f55348ad35304c956639e84d00c6eb8ed736
John Koelndorfer <jkoelndorfer@gmail.com>
2015-06-25 11:20:52 -0500

    zsh/custom/os/Darwin-post.zsh: Update EDITOR and VISUAL.
    Missed setting those to MacVim, which caused CLI applications relying on
    those variables to crash sometimes.

--- zsh/custom/os/Darwin-post.zsh
+++ zsh/custom/os/Darwin-post.zsh
@@ -3,7 +3,11 @@
 # Use GNU coreutils over BSD coreutils.
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH"
 
+vim_path='/Applications/MacVim.app/Contents/MacOS/Vim'
 # OS X's builtin version of vim has a tendency to crash, so let's use MacVim
 # instead.
-alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
-alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias vim="$vim_path"
+alias vi="$vim_path"
+
+export EDITOR="$vim_path"
+export VISUAL="$vim_path"
