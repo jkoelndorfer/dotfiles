ee96b67fbc075e9f78a3342d80a2f9443dc2c362
John Koelndorfer <jkoelndorfer@gmail.com>
2015-07-07 12:00:32 -0500

    zsh: On OS X, include Python binaries in PATH.

--- zsh/custom/os/Darwin-post.zsh
+++ zsh/custom/os/Darwin-post.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
 # Use GNU coreutils over BSD coreutils.
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH"
+PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH"
+
+# Include Python 2.7 binaries in PATH.
+PATH="$PATH:/Library/Frameworks/Python.framework/Versions/2.7/bin"
+
+export PATH
 
 vim_path='/Applications/MacVim.app/Contents/MacOS/Vim'
 # OS X's builtin version of vim has a tendency to crash, so let's use MacVim
