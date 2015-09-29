4e57d675d3e3a4a4f16c21c1a7716c591d07a866
John Koelndorfer <jkoelndorfer@gmail.com>
2015-09-29 09:14:09 -0500

    zsh: Skip loading theme if there is no tty.

--- zsh/custom/05-oh-my-zsh-preferences.zsh
+++ zsh/custom/05-oh-my-zsh-preferences.zsh
@@ -25,4 +25,10 @@ POWERLEVEL9K_PROMPT_ON_NEWLINE='true'
 POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
 POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX='%# '
 POWERLEVEL9K_TIME_FORMAT='%D{%F %H:%M:%S}'
-ZSH_THEME="powerlevel9k"
+
+# Some applications that start a shell but don't allocate a TTY
+# cause powerlevel9k to spit errors, so let's not load the theme
+# if that happens.
+if tty &> /dev/null; then
+    ZSH_THEME="powerlevel9k"
+fi
