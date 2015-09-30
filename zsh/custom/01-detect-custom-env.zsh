f3bcae0911935d22e84a7d062a5456e1ea068d0c
John Koelndorfer <jkoelndorfer@gmail.com>
2015-09-30 08:40:08 -0500

    zsh: Remove detection logic for zipnosis on a workstation.
    Use .zshenv.local to set the custom environment instead.

--- zsh/custom/01-detect-custom-env.zsh
+++ zsh/custom/01-detect-custom-env.zsh
@@ -4,4 +4,3 @@ typeset -p custom_env &> /dev/null || custom_env=()
 
 [[ -f '/etc/openwrt_release' ]] && custom_env+=('openwrt')
 hostname -f | grep -q 'zipnosis.com$' && custom_env+=('zipnosis')
-[[ "$(hostname -f)" = 'Johns-MacBook-Pro.local' ]] && custom_env+=('zipnosis')
