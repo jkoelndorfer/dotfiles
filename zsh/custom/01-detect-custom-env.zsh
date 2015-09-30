51d64f0a8fcc20465fd9c4af4ffba1a033b3220e
John Koelndorfer <jkoelndorfer@gmail.com>
2015-09-30 08:39:26 -0500

    zsh: Allow setting custom_env outside 01-detect-custom-env.zsh.

--- zsh/custom/01-detect-custom-env.zsh
+++ zsh/custom/01-detect-custom-env.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-custom_env=()
+typeset -p custom_env &> /dev/null || custom_env=()
 
 [[ -f '/etc/openwrt_release' ]] && custom_env+=('openwrt')
 hostname -f | grep -q 'zipnosis.com$' && custom_env+=('zipnosis')
