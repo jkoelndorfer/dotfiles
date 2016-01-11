796ee5a68c39d41d3cf95302547dbc1b850d1f06
John Koelndorfer <jkoelndorfer@gmail.com>
2016-01-11 09:06:39 -0600

    OS X: Use nvim if available, then MacVim, then system vim.

--- zsh/custom/os/Darwin-post.zsh
+++ zsh/custom/os/Darwin-post.zsh
@@ -7,11 +7,18 @@ pathmunge_reorder '/usr/local/opt/coreutils/libexec/gnubin'
 # Include Python 2.7 binaries in PATH.
 pathmunge '/Library/Frameworks/Python.framework/Versions/2.7/bin'
 
-vim_path='/Applications/MacVim.app/Contents/MacOS/Vim'
-# OS X's builtin version of vim has a tendency to crash, so let's use MacVim
-# instead.
-alias vim="$vim_path"
-alias vi="$vim_path"
+nvim_path="$(which nvim 2>/dev/null)"
+macvim_path='/Applications/MacVim.app/Contents/MacOS/Vim'
+
+# OS X's builtin version of vim has a tendency to crash, so let's use nvim
+# if available, else MacVim.
+if [[ -n "$nvim_path" ]]; then
+    alias vim="$nvim_path"
+    alias vi="$nvim_path"
+elif [[ -x "$macvim_path" ]]; then
+    alias vim="$macvim_path"
+    alias vi="$macvim_path"
+fi
 
 export EDITOR="$vim_path"
 export VISUAL="$vim_path"
