2d2eb4e9c73ab75a45c30b8b8edfeb8863df536d
John Koelndorfer <jkoelndorfer@gmail.com>
2016-09-23 22:35:42 -0500

    zsh: Move VIM_ variables to zshenv to allow running under Ansible.

--- zsh/custom/01-core.zsh
+++ zsh/custom/01-core.zsh
@@ -5,10 +5,6 @@ umask $DEFAULT_UMASK
 
 setopt vi
 
-export VIM_DIR="$REALHOME/.vim"
-export VIM_BUNDLE_DIR="$REALHOME/.vim/bundle"
-export VIM_RUNTIME_DIR="$DOTFILE_DIR/vim/vimdir/local"
-
 # Determine operating system.
 #
 # We're putting this in the environment because it might be useful to
