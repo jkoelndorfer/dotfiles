466b037d5caa2711b175baf87549eb7357bad0cf
John Koelndorfer <jkoelndorfer@gmail.com>
2016-10-06 10:34:53 -0500

    zsh: Fix Ctrl-S breaking my terminal output.

---
+++ zsh/custom/05-term-prefs.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Ctrl-S normally suspends terminal output. I end up hitting this a lot
+# by mistake and it's sorta irritating.
+#
+# Running `stty -ixon` disables this functionality.
+stty -ixon
