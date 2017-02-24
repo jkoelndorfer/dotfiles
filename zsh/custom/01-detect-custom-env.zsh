98f27495916ce879df1c2b95b34bb0cf67030f0e
John Koelndorfer <jkoelndorfer@gmail.com>
2017-02-23 22:31:34 -0600

    zsh: Set linux VT to solarized colors on login.

--- zsh/custom/01-detect-custom-env.zsh
+++ zsh/custom/01-detect-custom-env.zsh
@@ -4,3 +4,4 @@ typeset -p custom_env &> /dev/null || custom_env=()
 
 [[ -f '/etc/openwrt_release' ]] && custom_env+=('openwrt')
 hostname -f | grep -q 'zipnosis.com$' && custom_env+=('zipnosis')
+[[ "$TERM" == "linux" ]] && custom_env+=('linuxvt')
