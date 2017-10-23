181362067d0d911a39f06bf2173bc16e53a1e89c
John Koelndorfer <jkoelndorfer@gmail.com>
2017-10-22 19:54:18 -0500

    kde/configure-kde.sh: Configure basic KDE hotkeys.

--- kde/configure-kde.sh
+++ kde/configure-kde.sh
@@ -2,3 +2,6 @@
 
 konsole_profile="$HOME/.local/share/konsole/Shell.profile"
 [[ -f "$konsole_profile" ]] || cp "kde/konsole/Shell.profile" "$konsole_profile"
+
+hotkeys_file="$HOME/.config/khotkeysrc"
+[[ -f "$hotkeys_file" ]] || cp "kde/khotkeysrc" "$hotkeys_file"
