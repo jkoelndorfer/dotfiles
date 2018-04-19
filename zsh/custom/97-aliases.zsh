54442565fe9d447b047bf6e14564c3ae67c1a57f
John Koelndorfer <jkoelndorfer@gmail.com>
2018-04-19 14:55:06 -0500

    zsh: Add gb alias.

--- zsh/custom/97-aliases.zsh
+++ zsh/custom/97-aliases.zsh
@@ -21,6 +21,7 @@ alias rm='rm -i'
 # maintain two caches, so use `sudo yum` instead.
 alias yum='sudo yum'
 
+alias gb='git checkout "$(git branch -v | cut -c 3- | fzf | awk '"'"'{ print $1 }'"'"')"'
 alias gco='git checkout'
 alias gs='git status'
 alias gd='git diff'
