c735a8cd36ccc41020e134677c4d2fd0e4fd3a7c
John Koelndorfer <jkoelndorfer@gmail.com>
2018-04-27 10:17:46 -0500

    git: Use 'simple' for push.default.

--- git/configure-git.sh
+++ git/configure-git.sh
@@ -4,7 +4,7 @@ gcg='git config --global'
 
 $gcg color.ui 'auto'
 $gcg user.name 'John Koelndorfer'
-$gcg push.default 'upstream'
+$gcg push.default 'simple'
 
 # Use personal e-mail for the dotfiles repository.
 git config user.email 'jkoelndorfer@gmail.com'
