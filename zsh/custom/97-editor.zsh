be33232490aa38706982ecc79554a6814c5f3bfc
John Koelndorfer <jkoelndorfer@gmail.com>
2018-05-02 09:48:22 -0500

    neovim: Set VTE_VERSION=100 to prevent cursor changes.
    This prevents badly behaved plugins, like easymotion, from changing the
    cursor.

--- zsh/custom/97-editor.zsh
+++ zsh/custom/97-editor.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+# This tells neovim not to change the terminal cursor, no matter what.
+# Some poorly behaved neovim plugins may not abide by the guicursor
+# setting. This is a way of forcibly fixing that.
+#
+# easymotion, I'm looking at you.
+export VTE_VERSION=100
+
 if which nvim >/dev/null 2>&1; then
     nvim_path="$(which nvim)"
 fi
