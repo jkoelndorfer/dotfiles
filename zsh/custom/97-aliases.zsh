3f6cfb3ed698d4771a82fc3e1fead624d4816de6
John Koelndorfer <jkoelndorfer@gmail.com>
2018-05-17 09:00:21 -0500

    zsh: Make `gb` alias behave better.
    Previously, if you ran `gb` and ^C'd it, it would just revert all
    unstaged changes. Oops.

--- zsh/custom/97-aliases.zsh
+++ zsh/custom/97-aliases.zsh
@@ -62,13 +62,18 @@ alias rm='rm -i'
 # maintain two caches, so use `sudo yum` instead.
 alias yum='sudo yum'
 
-alias gb='git checkout "$(git branch -v | cut -c 3- | fzf | awk '"'"'{ print $1 }'"'"')"'
 alias gco='git checkout'
 alias gs='git status'
 alias gd='git diff'
 alias gds='git diff --staged'
 alias gpc='git status | less; git diff --staged'
 alias gc='git commit --verbose'
+
+function gb() {
+    branch="$(git branch -v | cut -c 3- | fzf | awk '{ print $1 }')"
+    [[ -n "$branch" ]] || return 1
+    git checkout "$branch"
+}
 
 function gpu() {
     git push -u origin "$(git current-branch)"
