05b31fe78819dc5dbe24b7667507370f8843c604
John Koelndorfer <jkoelndorfer@gmail.com>
2018-05-18 06:39:44 -0500

    zsh: Implement tnc alias.

--- zsh/custom/97-aliases.zsh
+++ zsh/custom/97-aliases.zsh
@@ -53,6 +53,22 @@ function tn() {
     fi
 }
 
+
+# This function implements a nifty shortcut for a common workflow pattern of mine.
+#
+# I often find myself organizing tmux sessions based on the directory I am in. Generally
+# my tmux sessions correspond to a project (git repo) that I'm working on. This will
+# prompt me to select a directory and attach to the tmux session associated with that
+# directory. If one does not exist, it will be created.
+function tnc() {
+    target_dir=$(selectdir)
+    session_name=$(basename "$target_dir")
+    if ! tmux-flexattach "$session_name" 2>/dev/null; then
+        (cd "$target_dir"; tmux new-session -d -s "$session_name")
+        tmux-flexattach "$session_name"
+    fi
+}
+
 alias ack="ack --color --pager='$PAGER'"
 alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -l --color=auto'
