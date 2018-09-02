307ce261ea240ec3ce14a9bd0a5b5f5296bf9bd4
John Koelndorfer <jkoelndorfer@gmail.com>
2018-09-02 11:21:46 -0500

    kde: Make configure-kde.sh work on systems without KDE.

--- kde/configure-kde.sh
+++ kde/configure-kde.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 konsole_profile="$HOME/.local/share/konsole/Shell.profile"
-[[ -f "$konsole_profile" ]] || cp "kde/konsole/Shell.profile" "$konsole_profile"
+mkdir -p "$(dirname "$konsole_profile")"
+[[ -f "$konsole_profile" ]] || cp "konsole/Shell.profile" "$konsole_profile"
 
 hotkeys_file="$HOME/.config/khotkeysrc"
-[[ -f "$hotkeys_file" ]] || cp "kde/khotkeysrc" "$hotkeys_file"
+mkdir -p "$(dirname "$hotkeys_file")"
+[[ -f "$hotkeys_file" ]] || cp "khotkeysrc" "$hotkeys_file"
 
-cp -f "kde/konsole/konsoleui.rc" "$HOME/.local/share/kxmlgui5/konsole/konsoleui.rc"
+consoleui_rc_file="$HOME/.local/share/kxmlgui5/konsole/konsoleui.rc"
+mkdir -p "$(dirname "$consoleui_rc_file")"
+cp -f "konsole/konsoleui.rc" "$consoleui_rc_file"
