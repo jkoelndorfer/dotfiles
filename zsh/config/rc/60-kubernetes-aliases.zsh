05128429005fb77e310c585c9c58db3e9c573f7b
John Koelndorfer <jkoelndorfer@gmail.com>
2019-01-22 11:47:29 -0600

    zsh: Add kubesh alias.

--- zsh/config/rc/60-kubernetes-aliases.zsh
+++ zsh/config/rc/60-kubernetes-aliases.zsh
@@ -4,3 +4,8 @@ function kubectx() {
     local selected_ctx=$(echo "$selected_ctx_line" | sed -r -e 's/^\*//' | awk '{ print $1 }')
     kubectl config use-context "$selected_ctx"
 }
+
+function kubesh() {
+    local selected_pod=$(kubectl get pods | fzf --header-lines=1 | awk '{ print $1 }')
+    kubectl exec -it "$selected_pod" bash
+}
