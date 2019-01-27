23ba48755ac1d01d42cea602944b5c0bb9ff0dc0
John Koelndorfer <jkoelndorfer@gmail.com>
2019-01-27 14:12:14 -0600

    zsh: Set KEYTIMEOUT=1 to reduce vi mode switch delay.

--- zsh/config/rc/50-sh-prefs.zsh
+++ zsh/config/rc/50-sh-prefs.zsh
@@ -13,5 +13,14 @@ export HISTSIZE=10000
 export SAVEHIST=$HISTSIZE
 
 set -o vi
+# Per https://dougblack.io/words/zsh-vi-mode.html:
+#
+# By default, there is a 0.4 second delay after you hit the <ESC> key and when
+# the [zsh vi mode] change is registered.
+#
+#
+# KEYTIMEOUT=1 reduces the delay to 0.1 seconds.
+export KEYTIMEOUT=1
+
 c_default_search_directories=("${c_default_search_directories[@]}" "$HOME/projects" "$HOME/src")
 c_addl_directories=("$DOTFILE_DIR")
