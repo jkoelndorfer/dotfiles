fa504ac539e965d6109c7bd019ad79b35aaf58e5
John Koelndorfer <jkoelndorfer@gmail.com>
2019-01-30 19:43:34 -0600

    fzf: Set default opts in a wrapper script instead of using zshrc.
    fzf colors were not being set correctly all the time when called from
    tmux.

--- zsh/config/rc/50-fzf.zsh
+++
@@ -1,8 +0,0 @@
-source "$DOTFILE_DIR/colors/solarized-dark"
-
-export FZF_DEFAULT_OPTS="
-    --color fg:-1,bg:-1,hl:$SOLARIZED_BASE3_TERM16,fg+:$SOLARIZED_BASE3_TERM16,bg+:$SOLARIZED_BASE1_TERM16,hl+:$SOLARIZED_BASE3_TERM16
-    --color info:$SOLARIZED_YELLOW_TERM16,prompt:$SOLARIZED_BLUE_TERM16,pointer:$SOLARIZED_BASE3_TERM16
-    --color marker:$SOLARIZED_BASE3_TERM16,spinner:$SOLARIZED_BLUE_TERM16
-    --no-bold
-"
