f06eab7a27ff49120705dcba96540df20ab26968
John Koelndorfer <jkoelndorfer@gmail.com>
2019-03-17 15:18:49 -0500

    zsh: Remove legacy wine configuration.

--- zsh/config/rc/50-wine.zsh
+++
@@ -1,6 +0,0 @@
-# Use 32-bit wineprefixes by default. Offers best compatibility with most
-# software.
-export WINEARCH='win32'
-
-# Disable all wine debugging messages. This provides a slight performance boost.
-export WINEDEBUG='-all'
