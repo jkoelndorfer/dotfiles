ef679c1048a5f094a9a6af4cecab4389981494f3
John Koelndorfer <jkoelndorfer@gmail.com>
2019-04-19 18:50:20 -0500

    deathwing: Disable DPMS, configure display names.

--- zsh/config/hostenv/deathwing/50-display.zsh
+++ zsh/config/hostenv/deathwing/50-display.zsh
@@ -1,2 +1,4 @@
-export DISPLAYNAME_CENTER='DisplayPort-0'
-export DISPLAYNAME_RIGHT='HDMI-A-0'
+export DISPLAYNAME_LEFT='DisplayPort-0'
+export DISPLAYNAME_CENTER='DisplayPort-1'
+export DISPLAYNAME_RIGHT='DisplayPort-2'
+export X11_DISABLE_DPMS=1
