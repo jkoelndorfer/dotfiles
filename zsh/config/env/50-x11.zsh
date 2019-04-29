3b7c51e8762f7e8c316727bf503fae66585a021b
John Koelndorfer <jkoelndorfer@gmail.com>
2019-04-29 08:53:11 -0500

    lockscreen: Don't resume playing music after unlocking after 20 minutes.

--- zsh/config/env/50-x11.zsh
+++ zsh/config/env/50-x11.zsh
@@ -1,6 +1,13 @@
 # Time, in minutes, before xautolock will lock the screen.
 export SCREENLOCK_TIME_MINUTES=15
 
+# Maximum time, in seconds, that the screen can be locked and
+# still resume music if there was any playing.
+#
+# This will keep Spotify from playing music on the Echo speakers
+# at home when I show up at the office in the morning.
+export SCREENLOCK_MUSIC_RESUME_TIME_SECONDS=1200
+
 # Time, in seconds, after which the mouse cursor will be hidden
 # if the mouse is not moved.
 export CURSOR_HIDE_TIME_SECONDS=5
