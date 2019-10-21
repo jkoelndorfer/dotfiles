91487f9a8dd5fcde59afc08e3a884d4062855939
John Koelndorfer <jkoelndorfer@gmail.com>
2019-10-21 12:24:46 -0500

    zsh/config/rc/60-aws-aliases.zsh: Clean up function names.

--- zsh/config/rc/60-aws-aliases.zsh
+++ zsh/config/rc/60-aws-aliases.zsh
@@ -1,14 +1,14 @@
 # Dumps out all of the EC2 instances with the given name.
-function ec2_instances_named() {
+function aws-ec2-instances-named() {
     local name="$1"
     aws ec2 describe-instances --filters "Name=tag:Name,Values=$name"
 }
 
-function ec2_instance_names() {
+function aws-ec2-instance-names() {
     aws ec2 describe-instances --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' | jq -r '.[][0][0]' | sort -u
 }
 
-function ec2_instance_public_ip() {
+function aws-ec2-instance-public-ip() {
     local instance_id=$1
 
     aws ec2 describe-instances \
@@ -17,33 +17,41 @@ function ec2_instance_public_ip() {
         --output text
 }
 
-function lsasg() {
+function aws-ami-select() {
+    local tab="$(echo -e '\t')"
+    {
+        echo -e "Name\tImage ID\tCreation Date"
+        aws ec2 describe-images --owners self | jq -r '.Images[] | [.Name, .ImageId, .CreationDate] | join("\t")' | sort -k3 -r -t "$tab"
+    } | column -t -o "$tab" -s "$tab" | fzf --header-lines 1 | awk -F"$tab" '{ print $2 }'
+}
+
+function aws-asg-ls() {
     aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName' --output text |
         sed -e 's/\t/\n/g' | sort
 }
 
-function asgmin() {
+function aws-asg-set-min() {
     local asg_name=$1
     local min_size=$2
 
     aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --min-size "$min_size"
 }
 
-function asgdesired() {
+function aws-asg-set-desired() {
     local asg_name=$1
     local desired_capacity=$2
 
     aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --desired-capacity "$desired_capacity"
 }
 
-function asgmax() {
+function aws-asg-set-max() {
     local asg_name=$1
     local max_size=$2
 
     aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --max-size "$max_size"
 }
 
-function asginstances() {
+function aws-asg-get-instances() {
     local asg_name=$1
 
     aws autoscaling describe-auto-scaling-groups \
@@ -52,32 +60,41 @@ function asginstances() {
         --output text | sed -e 's/\t/\n/g'
 }
 
-function asg_each() {
+function aws-asg-ssh-each() {
     local asg_name=$1
     local action=$2
 
-    local instances=$(asginstances "$asg_name")
+    local instances=$(aws-asg-get-instances "$asg_name")
     while read i; do
-        local ip=$(ec2_instance_public_ip "$i")
+        local ip=$(aws-ec2-instance-public-ip "$i")
 
         ssh -n -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$ip" "$action"
     done <<<"$instances"
 }
 
-function lsssmp() {
-    aws ssm describe-parameters --query 'Parameters[].Name' --output text | sed -e 's/\t/\n/g' | sort
+function aws-lt-set-ami() {
+    local lt_name=$1
+    local ami_id=$2
+
+    aws ec2 create-launch-template-version --launch-template-name "$lt_name" --source-version '$Latest' --launch-template-data '{"ImageId": "'"$ami_id"'"}'
 }
 
-function ssmp() {
+function aws-ssm-parameter-ls() {
+    aws ssm describe-parameters --query 'Parameters[].Name' --output text | sed -e 's/\t/\n/g' | sort
+}
+alias ls-ssmp=aws-ssm-parameter-ls
+
+function aws-ssm-parameter() {
     if [[ -n "$1" ]]; then
         local name=$1
     else
-        local name=$(select_ssm_param)
+        local name=$(aws-ssm-parameter-select)
     fi
     aws ssm get-parameter --name "$name" --with-decryption | jq -r '.Parameter.Value'
 }
+alias ssmp=aws-ssm-parameter
 
-function select_ssm_param() {
+function aws-ssm-parameter-select() {
     local tab=$(echo -e '\t')
     aws ssm describe-parameters |
         jq -r '.Parameters[] | (.Name + "\t" + .Description)' |
