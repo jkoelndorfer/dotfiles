bb610a6dd2f6bd5406a664643e52206ad14a51ef
John Koelndorfer <jkoelndorfer@gmail.com>
2019-12-16 13:28:01 -0600

    zsh: Automatically include color definitions.

---
+++ zsh/config/rc/01-color-lib.zsh
@@ -0,0 +1,11 @@
+# zsh provides handy color definitions for you out of the box!
+#
+# This will permit you to use $fg, $fg_bold, $bg, $bg_bold, $reset_color,
+# and $bold_color.
+#
+# For example:
+# echo "${fg[red]}red text${reset_color} plain text"
+#
+# See https://github.com/zsh-users/zsh/blob/master/Functions/Misc/colors.
+autoload colors
+colors
