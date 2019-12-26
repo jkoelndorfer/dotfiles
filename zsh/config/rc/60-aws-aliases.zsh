f0f5f3c53204f6bd51019a224ef7edc62fdbe04b
John Koelndorfer <jkoelndorfer@gmail.com>
2019-12-26 09:19:37 -0600

    WIP: Refactor AWS aliases.

--- zsh/config/rc/60-aws-aliases.zsh
+++ zsh/config/rc/60-aws-aliases.zsh
@@ -8,6 +8,33 @@ function aws-columnize() {
     column -t -s "$tab" -o "$tab"
 }
 
+function aws-field() {
+    local field_number=$1
+    awk -F "$tab" "{ print \$${field_number} }"
+}
+
+function aws-has-fields() {
+    [[ "$1" =~ $tab ]]
+}
+
+function aws-select() {
+    local field_number=$1
+    fzf --header-lines 1 | aws-field "$field_number"
+}
+
+function aws-sort() {
+    sort -t "$tab" "$@"
+}
+
+function aws-present-header() {
+    while [[ -n "$1" ]]; do
+        printf "$1"
+        [[ -n "$2" ]] && printf "$tab"
+        shift
+    done
+    echo
+}
+
 function aws-ec2-instance-describe-short() {
     local jq_query
     read -rd '' jq_query <<'EOF'
@@ -26,87 +53,103 @@ EOF
 
 function aws-ec2-instance-ls() {
     {
-        echo -e "Name\tID\tPublic IP\tAvailability Zone\tLaunch Time"
+        aws-present-header 'Name' 'ID' 'Public IP' 'Availability Zone' 'Launch Time'
         aws-ec2-instance-describe-short | jq -r '[.name, .instance_id, .public_ip, .availability_zone, .launch_time] | join("\t")'
     } | aws-columnize
 }
 
-# Dumps out all of the EC2 instances with the given name.
-function aws-ec2-instances-named() {
-    local name="$1"
-    aws ec2 describe-instances --filters "Name=tag:Name,Values=$name"
-}
-
-function aws-ec2-instance-names() {
-    aws ec2 describe-instances --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value' | jq -r '.[][0][0]' | sort -u
-}
-
 function aws-ec2-instance-select() {
-    aws-ec2-instance-ls | fzf --header-lines 1
+    aws-ec2-instance-ls | aws-select
+}
+
+function aws-ec2-instance-select-id() {
+    aws-ec2-instance-select | aws-field 2
 }
 
 function aws-ec2-instance-terminate() {
-    local selected_instance=$(aws-ec2-instance-select)
-    [[ -n "$selected_instance" ]] || return 1
-    local instance_name=$(echo "$selected_instance" | awk -F"$tab" '{ print $1 }' | trim-string)
-    local instance_id=$(echo "$selected_instance" | awk -F"$tab" '{ print $2 }' | trim-string)
+    local instance_id=$1
+    local instance_name=$2
+    if [[ -n "$instance_name" ]]; then
+        local confirm_msg="terminate instance $instance_name ($instance_id)"
+    else
+        local confirm_msg="terminate instance $instance_id"
+    fi
+    confirm-cmd "$confirm_msg" aws ec2 terminate-instances --instance-ids "$instance_id"
+}
 
-    confirm-cmd "terminate instance $instance_name ($instance_id)" aws ec2 terminate-instances --instance-ids "$instance_id"
+function aws-ec2-instance-select-terminate() {
+    local selected_instance=$(aws-ec2-instance-select)
+    local instance_name=$(echo "$selected_instance" | aws-field 1 | trim-string)
+    local instance_id=$(echo "$selected_instance" | aws-field 2 | trim-string)
+    aws-ec2-instance-terminate "$instance_id" "$instance_name"
 }
 
 function aws-ec2-instance-public-ip() {
-    local instance_id=$1
+    local instance_id=$(value-or-cmd "$1" aws-ec2-instance-select-id)
 
     aws ec2 describe-instances \
         --instance-id "$instance_id" \
         --query 'Reservations[0].Instances[0].PublicIpAddress' \
-        --output text
+        --output text | trim-string
 }
 
 function aws-ec2-security-group-ls() {
     {
-        echo -e "Group Name\tGroup ID\tVPC ID"
+        aws-present-header 'Group Name' 'Group ID' 'VPC ID'
         aws ec2 describe-security-groups | jq -r '.SecurityGroups[] | [.GroupName, .GroupId, .VpcId] | join("\t")' | sort
     } | aws-columnize
 }
 
 function aws-ec2-security-group-select() {
-    aws-ec2-security-group-ls | fzf --header-lines 1
+    aws-ec2-security-group-ls | aws-select
 }
 
 function aws-ec2-security-group-select-id() {
-    aws-ec2-security-group-select | awk -F"$tab" '{ print $2 }'
+    aws-ec2-security-group-select | aws-field 2
 }
 
 function aws-ec2-security-group-delete() {
-    local security_group_defn=$(aws-ec2-security-group-select)
-    [[ -n "$security_group_defn" ]] || return 1
-    security_group_name=$(echo "$security_group_defn" | awk -F"$tab" '{ print $1 }' | trim-string)
-    security_group_id=$(echo "$security_group_defn" | awk -F"$tab" '{ print $2 }' | trim-string)
-    confirm-cmd "delete security group $security_group_name ($security_group_id)" aws ec2 delete-security-group --group-id "$security_group_id"
+    local security_group_defn=$(value-or-cmd "$1" aws-ec2-security-group-select)
+    if aws-has-fields "$security_group_defn"; then
+        security_group_name=$(echo "$security_group_defn" | aws-field 1 | trim-string)
+        security_group_id=$(echo "$security_group_defn" | aws-field 2 | trim-string)
+        confirm_msg="delete security group $security_group_name ($security_group_id)"
+    else
+        security_group_id=$security_group_defn
+        confirm_msg="delete security group $security_group_id"
+    fi
+    confirm-cmd "$confirm_msg" aws ec2 delete-security-group --group-id "$security_group_id"
 }
 
-function _aws-ami-ls() {
-    aws ec2 describe-images --owners self | jq -r '.Images[] | [.Name, .ImageId, .CreationDate] | join("\t")' | sort -k3 -r -t "$tab"
+function aws-ec2-subnet-ls() {
+    local jq_query
+    read -rd '' jq_query <<'EOF'
+    .Subnets[] |
+        [(.Tags[] | select(.Key == "Name") | .Value) // "-", .SubnetId, .CidrBlock, .VpcId, .AvailabilityZone] |
+        join("\t")
+EOF
+    {
+        aws-present-header 'Name' 'Subnet ID' 'CIDR Block' 'VPC ID' 'Availability Zone'
+        aws ec2 describe-subnets | jq -r "$jq_query" | sort
+    } | aws-columnize
 }
 
 function aws-ami-ls() {
-    _aws-ami-ls | aws-columnize
+    {
+        aws-present-header 'Name' 'Image Id' 'Creation Date'
+        aws ec2 describe-images --owners self |
+            jq -r '.Images[] | [.Name, .ImageId, .CreationDate] | join("\t")' |
+            aws-sort -k3 -r
+    } | aws-columnize
 }
 
 function aws-ami-select() {
-    {
-        echo -e "Name\tImage ID\tCreation Date"
-        _aws-ami-ls
-    } | aws-columnize | fzf --header-lines 1 | awk -F"$tab" '{ print $2 }'
+    aws-ami-ls | aws-select
 }
 
 function aws-ami-snapshot-ls() {
-    local image_id=$1
-    if [[ -z "$image_id" ]]; then
-        echo "$0: image ID must be specified" >&2
-        return 1
-    fi
+    local image_id=$(value-or-cmd "$1" aws-ami-select)
+    [[ -n "$image_id" ]] || return 1
     aws ec2 describe-images \
         --image-ids "$image_id" \
         --query 'Images[*].BlockDeviceMappings[*].Ebs.SnapshotId' \
@@ -114,7 +157,7 @@ function aws-ami-snapshot-ls() {
 }
 
 function aws-ami-rm() {
-    local image_id=$1
+    local image_id=$(value-or-cmd "$1" aws-ami-select)
     local image_snapshots=($(aws-ami-snapshot-ls "$image_id"))
     if [[ -z "$image_snapshots" ]]; then
         echo "$0: Could not determine snapshots for AMI ID $image_id; is the ID correct?" >&2
@@ -127,8 +170,24 @@ function aws-ami-rm() {
 }
 
 function aws-asg-ls() {
-    aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[*].AutoScalingGroupName' --output text |
-        aws-canonicalize-text | sort
+    local jq_query
+    read -rd '' jq_query <<'EOF'
+    .AutoScalingGroups[] | [
+        .AutoScalingGroupName,
+        (.Instances | length),
+        ([.Instances[] | select(.LifecycleState == "InService")] | length),
+        .MinSize,
+        .DesiredCapacity,
+        .MaxSize,
+        (.AvailabilityZones | join(", "))
+    ] | join("\t")
+EOF
+    {
+        aws-present-header 'Autoscaling Group Name' '# Instances' '# InService' 'Min' 'Desired' 'Max' 'Availability Zones'
+        aws autoscaling describe-auto-scaling-groups |
+            jq -r "$jq_query" |
+            aws-sort
+    } | aws-columnize
 }
 
 function aws-asg-set-min() {
@@ -173,19 +232,16 @@ function aws-asg-ssh-each() {
     done <<<"$instances"
 }
 
-function _aws-lt-ls() {
-    aws ec2 describe-launch-templates | jq -r '.LaunchTemplates[] | [.LaunchTemplateName, .LaunchTemplateId, .LatestVersionNumber] | join("\t")'
-}
-
 function aws-lt-ls() {
     {
-        echo -e 'Launch Template Name\tLaunch Template ID\tLatest Version'
-        _aws-lt-ls
+        aws-present-header 'Launch Template Name' 'Launch Template ID' 'Latest Version'
+        aws ec2 describe-launch-templates |
+            jq -r '.LaunchTemplates[] | [.LaunchTemplateName, .LaunchTemplateId, .LatestVersionNumber] | join("\t")'
     } | aws-columnize
 }
 
 function aws-lt-select() {
-    aws-lt-ls | fzf --header-lines 1 | awk -F"$tab" '{ print $2 }'
+    aws-lt-ls | aws-select | aws-field 2
 }
 
 function aws-lt-set-ami() {
@@ -196,7 +252,10 @@ function aws-lt-set-ami() {
 }
 
 function aws-ssm-parameter-ls() {
-    aws ssm describe-parameters --query 'Parameters[].Name' --output text | aws-canonicalize-text | sort
+    {
+        aws-present-header 'Name' 'Description'
+        aws ssm describe-parameters | jq -r '.Parameters[] | [.Name, .Description] | join("\t")'
+    } | aws-columnize
 }
 alias ls-ssmp=aws-ssm-parameter-ls
 
@@ -204,14 +263,19 @@ function aws-ssm-parameter() {
     if [[ -n "$1" ]]; then
         local name=$1
     else
-        local name=$(aws-ssm-parameter-select)
+        local name=$(aws-ssm-parameter-select-name)
     fi
     aws ssm get-parameter --name "$name" --with-decryption | jq -r '.Parameter.Value'
 }
 alias ssmp=aws-ssm-parameter
 
 function aws-ssm-parameter-select() {
-    aws ssm describe-parameters |
-        jq -r '.Parameters[] | (.Name + "\t" + .Description)' |
-        aws-columnize | fzf | awk '{ print $1 }'
+    {
+        aws ssm describe-parameters |
+            jq -r '.Parameters[] | (.Name + "\t" + (.Description // "-"))'
+    } | aws-columnize | aws-select
+}
+
+function aws-ssm-parameter-select-name() {
+    aws-ssm-parameter-select | aws-field 1
 }
