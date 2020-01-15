40353e672038c8655f2888c950843b0f5ab074aa
John Koelndorfer <jkoelndorfer@gmail.com>
2020-01-15 10:24:30 -0600

    zsh: Add helper for vagrant on johnk-aeris

---
+++ zsh/config/rc/90-aeris.zsh
@@ -0,0 +1,13 @@
+[[ "$(hostname -s)" != 'johnk-aeris' ]] && return
+
+function vagrant() {
+    if [[ "$1" != 'ssh' ]]; then
+        command vagrant "$@"
+        return "$?"
+    fi
+    # On johnk-aeris, vagrant test VMs will be provisioned with our company
+    # username and password. Since that will account include shell preferences,
+    # let's connect that way instead of using the default 'vagrant'.
+    hostname=$(command vagrant ssh-config | awk '$1 == "HostName" { print $2 }')
+    ssh "$hostname"
+}
