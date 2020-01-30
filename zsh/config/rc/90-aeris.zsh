ba93ff764da6d6a8596c5b2b540359dcee19cac8
John Koelndorfer <jkoelndorfer@gmail.com>
2020-01-30 14:36:15 -0600

    zsh: Use local in vagrant function

--- zsh/config/rc/90-aeris.zsh
+++ zsh/config/rc/90-aeris.zsh
@@ -8,6 +8,6 @@ function vagrant() {
     # On johnk-aeris, vagrant test VMs will be provisioned with our company
     # username and password. Since that will account include shell preferences,
     # let's connect that way instead of using the default 'vagrant'.
-    hostname=$(command vagrant ssh-config | awk '$1 == "HostName" { print $2 }')
+    local hostname=$(command vagrant ssh-config | awk '$1 == "HostName" { print $2 }')
     ssh "$hostname"
 }
