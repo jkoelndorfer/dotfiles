aa5285cf1b5380dce61e10c4cad9d228a589eab5
John Koelndorfer <jkoelndorfer@gmail.com>
2020-12-14 13:05:32 -0600

    git: Add fix-authorship alias to fix commit authorship information

--- git/configure-git.sh
+++ git/configure-git.sh
@@ -11,6 +11,7 @@ git config user.email 'jkoelndorfer@gmail.com'
 
 git config --global alias.current-branch 'rev-parse --abbrev-ref HEAD'
 git config --global alias.delete-branch "!~/dotfiles/git/delete-branch"
+git config --global alias.fix-authorship "!~/dotfiles/git/fix-authorship"
 git config --global alias.new-branch '!sh -c "git fetch; git checkout -b $1 origin/master"'
 git config --global alias.root 'rev-parse --show-toplevel'
 git config --global alias.fzf-log 'log --color=always --pretty=format:"%C(yellow)%h %C(green)%an %C(blue)%ad %C(reset)%s" --date=iso --abbrev-commit'
