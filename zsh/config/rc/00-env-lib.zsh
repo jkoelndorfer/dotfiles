56191290727a6ee79aafe134a52d0f5621d8dbe1
John Koelndorfer <jkoelndorfer@gmail.com>
2021-05-05 20:58:55 -0500

    zsh: Make pathmunge functions more generic

--- zsh/config/rc/00-env-lib.zsh
+++ zsh/config/rc/00-env-lib.zsh
@@ -6,17 +6,53 @@
 # that after adding confirm-cmd to my zsh configuration, lightdm would no longer start i3.
 #
 # It's probably best to make sure anything in this file is strictly POSIX shell compliant.
-function pathmunge() {
-    if ! echo $PATH | grep -E -q "(^|:)$1($|:)"; then
-        if [ "$2" = "after" ] ; then
-            PATH=$PATH:$1
+function pathvarmunge() {
+    local s=$1
+    local addn=$2
+    local mode=$3
+
+    local addn_escaped=$(re_escape "$addn" '' 'extended')
+    if ! echo "$s" | grep -E -q "(^|:)$addn_escaped($|:)"; then
+        if [ "$mode" = "after" ] ; then
+            echo "$s:$addn"
         else
-            PATH=$1:$PATH
+            echo "$addn:$s"
         fi
+    else
+        echo "$s"
     fi
 }
 
+function pathvarmunge_reorder() {
+    local s=$1
+    local addn=$2
+    local mode=$3
+
+    local addn_escaped=$(re_escape "$addn" '#' 'extended')
+    local s_stripped="$(echo "$s" | sed -E -e "s#(^|:)$addn_escaped($|:)#:#g" | sed -E -e 's/(^:|:$)//')"
+    pathvarmunge "$s_stripped" "$addn" "$mode"
+}
+
+function pathmunge() {
+    export PATH=$(pathvarmunge "$PATH" "$@")
+}
+
 function pathmunge_reorder() {
-    PATH="$(echo "$PATH" | sed -E -e "s#(^|:)$1($|:)#:#g" | sed -E -e 's/(^:|:$)//')"
-    pathmunge "$@"
+    export PATH=$(pathvarmunge_reorder "$PATH" "$@")
+}
+
+# This regex escape function is borrowed from:
+# https://backreference.org/2009/12/09/using-shell-variables-in-sed/
+function re_escape() {
+    local s=$1
+    local sed_separator=$2
+    local re_mode=$3
+
+    local sed_escape_re
+    if [[ "$re_mode" == 'basic' ]]; then
+        sed_escape_re='s/[[\.*^$'$sed_separator']/\\&/g'
+    elif [[ "$re_mode" == 'extended' ]]; then
+        sed_escape_re='s/[[\.*^$(){}?+|'"$sed_separator"']/\\&/g'
+    fi
+    printf "%s\n" "$s" | sed -e "$sed_escape_re"
 }
