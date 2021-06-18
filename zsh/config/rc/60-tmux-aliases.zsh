b400935d31b4805f69066d0c6a3d02c569db41a8
John Koelndorfer <jkoelndorfer@gmail.com>
2021-06-18 10:01:32 -0500

    zsh: Update tmux helper functions to comply with current conventions

--- zsh/config/rc/60-tmux-aliases.zsh
+++ zsh/config/rc/60-tmux-aliases.zsh
@@ -1,9 +1,13 @@
-function tmux-flexattach() {
-    session="$1"
+# Attaches to the given tmux session.
+#
+# Works whether or not the current tty is
+# connected to a tmux session or not.
+function tmux-flex-attach() {
+    local session="$1"
     if [[ -n "$TMUX" ]]; then
-        tmux_cmd=(tmux switch-client -t)
+        local tmux_cmd=(tmux switch-client -t)
     else
-        tmux_cmd=(tmux attach-session -t)
+        local tmux_cmd=(tmux attach-session -t)
     fi
     [[ -n "$session" ]] || return 1
     if ! tmux has-session -t "$session"; then
@@ -12,10 +16,13 @@ function tmux-flexattach() {
     "${tmux_cmd[@]}" "$session"
 }
 
-function ta() {
-    selected_session_desc="$(tmux list-session | fzf)"
-    selected_session="$(echo "$selected_session_desc" | awk -F: '{ print $1 }')"
-    tmux-flexattach "$selected_session"
+# Displays a list of available tmux sessions for
+# selection via fzf, then attaches to the selected
+# session.
+function tmux-fzf-attach() {
+    local selected_session_desc="$(tmux list-session | fzf)"
+    local selected_session="$(echo "$selected_session_desc" | awk -F: '{ print $1 }')"
+    tmux-flex-attach "$selected_session"
 }
 
 # If the given session does not exist, create it.
@@ -23,22 +30,24 @@ function ta() {
 # The session is attached to.
 function tmux-new-or-attach() {
     local session="$1"
-    pushd "$HOME" >& /dev/null
     if tmux has-session -t "$session"; then
-        tmux-flexattach "$session"
+        tmux-flex-attach "$session"
     else
-        tn "$session"
+        tmux-flex-new-session "$session"
     fi
-    popd >& /dev/null
 }
 
-function tn() {
-    session="$1"
+# Creates a new tmux session.
+#
+# Works whether or not the current tty is
+# connected to a tmux session or not.
+function tmux-flex-new-session() {
+    local session="$1"
     pushd "$HOME" >& /dev/null
     if [[ -n "$TMUX" ]]; then
-        detached_arg='-d'
+        local detached_arg='-d'
     else
-        detached_arg=''
+        local detached_arg=''
     fi
     if [[ -n "$session" ]]; then
         tmux new-session $detached_arg -s "$session"
@@ -54,8 +63,10 @@ function tn() {
 # Reloads the environment from tmux. Useful for updating shell prompt
 # when working in a remote session or updating ssh-agent environment
 # variables, for example.
-function tenv() {
+function tmux-reload-environment() {
     eval "$(tmux show-environment -s)"
 }
 
 alias ti="tmux run-shell 'echo #{session_name}:#{window_index}.#{pane_index}'"
+alias ta="tmux-fzf-attach"
+alias tenv="tmux-reload-environment"
