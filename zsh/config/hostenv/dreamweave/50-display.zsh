6ffaee4d716f53eff7a6d20b2cc9b13164b7f01c
John Koelndorfer <jkoelndorfer@gmail.com>
2021-09-20 09:56:56 -0500

    dreamweave: Set DISPLAYNAME variables dependent on X11/Wayland session
    This is a hack and needs fixing later.

--- zsh/config/hostenv/dreamweave/50-display.zsh
+++ zsh/config/hostenv/dreamweave/50-display.zsh
@@ -1,4 +1,24 @@
-export DISPLAYNAME_LEFT='DP-1'
-export DISPLAYNAME_CENTER='DP-2'
-export DISPLAYNAME_RIGHT='DP-3'
-export SCREENSAVER_STRATEGY='none'
+# FIXME: This is a temporary hack to support different
+# display names under Wayland and X11.
+#
+# This is not a good solution. Install-time configuration
+# depends on the values of these variables, so installing
+# Wayland configuration will be broken when installing in
+# an X11 session and vice-versa.
+#
+# Perhaps something like
+#
+# DISPLAYNAME_WAYLAND_RIGHT, DISPLAYNAME_X11_RIGHT for install-time
+# configurations and DISPLAYNAME_RIGHT for anything at runtime?
+#
+# Need to think more on it.
+if [[ -n "$WAYLAND_DISPLAY" ]]; then
+    export DISPLAYNAME_LEFT='DP-1'
+    export DISPLAYNAME_CENTER='DP-2'
+    export DISPLAYNAME_RIGHT='DP-3'
+else
+    export DISPLAYNAME_LEFT='DisplayPort-0'
+    export DISPLAYNAME_CENTER='DisplayPort-1'
+    export DISPLAYNAME_RIGHT='DisplayPort-2'
+fi
+export SCREENSAVER_STRATEGY='screen-off'
