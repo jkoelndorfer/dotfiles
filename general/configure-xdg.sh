74acc8d5737d3cbf70749c5d3328c5d45cb9d5e9
John Koelndorfer <jkoelndorfer@gmail.com>
2022-04-24 11:24:12 -0500

    Use firefox as the default web browser
    Some sites don't play well with qutebrowser.

--- general/configure-xdg.sh
+++ general/configure-xdg.sh
@@ -4,4 +4,4 @@ if ! type xdg-settings >/dev/null 2>&1; then
     exit 0
 fi
 
-xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
+xdg-settings set default-web-browser firefox.desktop
