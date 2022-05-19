17c90161e90f15a153115eddc7837f7539f36acb
John Koelndorfer <jkoelndorfer@gmail.com>
2022-05-19 18:16:31 -0500

    zsh: Fix up clipboard aliases

---
+++ zsh/config/rc/60-clipboard-aliases.zsh
@@ -0,0 +1,2 @@
+alias cbcopy="$DOTFILE_DIR/bin/gui/copy-to-clipboard"
+alias cbpaste="$DOTFILE_DIR/bin/gui/paste-from-clipboard"
--- zsh/config/rc/60-x11-aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias cbcopy="$HOME/dotfiles/bin/copy-to-clipboard"
-alias cbpaste="$HOME/dotfiles/bin/paste-from-clipboard"
