31053c307a9c6c97d39de7f62b8feabfd5d1fd0e
John Koelndorfer <jkoelndorfer@gmail.com>
2022-05-19 21:29:39 -0500

    vim: Use run = 'make' to set up telescope-fzf-native

--- vim/config/50-packer.lua
+++ vim/config/50-packer.lua
@@ -84,7 +84,7 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = {
       {'nvim-lua/plenary.nvim'},
-      {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+      {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     }
   }
 
