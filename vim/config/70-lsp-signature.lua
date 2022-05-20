d24755322e92c20336c11044a198f436e0d7cf45
John Koelndorfer <jkoelndorfer@gmail.com>
2022-05-19 20:15:32 -0500

    vim: Convert 70-lsp-signature.vim to lua

--- vim/config/70-lsp-signature.lua
+++ vim/config/70-lsp-signature.lua
@@ -1,9 +1,6 @@
-lua <<EOF
-
 vim.g.lsp_signature_config = {
     verbose = false,
     log_path = vim.fn.stdpath("cache") .. "/lsp-signature.log",
     hint_enable = false,
     always_trigger = true,
 }
-EOF
