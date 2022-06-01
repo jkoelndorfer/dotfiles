bd9c4deb2318e27ba693c917d72ae0a17b0ac00e
John Koelndorfer <john@johnk.io>
2022-05-31 21:55:03 -0500

    nord: Add named nord colors to vim-colors

--- theme/nord/vim-colors.lua
+++ theme/nord/vim-colors.lua
@@ -1,3 +1,22 @@
+local nord_theme = {
+  nord0 = '#2e3440',
+  nord1 = '#3b4252',
+  nord2 = '#434c5e',
+  nord3 = '#4c566a',
+  nord4 = '#d8dee9',
+  nord5 = '#e5e9f0',
+  nord6 = '#eceff4',
+  nord7 = '#8fbcbb',
+  nord8 = '#88c0d0',
+  nord9 = '#81a1c1',
+  nord10 = '#5e81ac',
+  nord11 = '#bf616a',
+  nord12 = '#d08770',
+  nord13 = '#ebcb8b',
+  nord14 = '#a3be8c',
+  nord15 = '#b48ead',
+}
+
 if vim.env.TERM ~= 'linux' then
   vim.cmd('silent! colorscheme nord')
 
@@ -10,8 +29,8 @@ vim.api.nvim_set_option('termguicolors', true)
 local lualine_theme = require('lualine.themes.nord')
 
 -- Make lualine consistent with the tmux status line.
-lualine_theme.normal.a.bg = '#81a1c1'
-lualine_theme.normal.b.bg = '#4c566a'
-lualine_theme.normal.c.bg = '#3b4252'
+lualine_theme.normal.a.bg = nord_theme.nord9
+lualine_theme.normal.b.bg = nord_theme.nord3
+lualine_theme.normal.c.bg = nord_theme.nord1
 
 vim.g.lualine_theme = lualine_theme
