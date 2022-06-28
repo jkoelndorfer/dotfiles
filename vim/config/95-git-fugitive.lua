b5a5327fe3c5ffbcb455cd9b03104babc03256b5
John Koelndorfer <john@johnk.io>
2022-06-28 14:18:37 -0500

    vim: Quit if the implicit fugitive tab is the last one

--- vim/config/95-git-fugitive.lua
+++ vim/config/95-git-fugitive.lua
@@ -12,7 +12,28 @@ function configure_fugitive_tab()
   end
   vim.cmd('LualineRenameTab git')
   vim.cmd('tabmove -1')
+  vim.b.implicit_fugitive_buf = true
   vim.cmd('tabnext')
+end
+
+-- If the implicit fugitive's :Git is the last visible window,
+-- quit vim automatically.
+function exit_if_fugitive_window_last()
+  only_has_fugitive_buffer = true
+  for i, win in pairs(vim.api.nvim_list_wins()) do
+    buf = vim.api.nvim_win_get_buf(win)
+    status, result = pcall(function() return vim.api.nvim_buf_get_var(buf, 'implicit_fugitive_buf') end)
+    if status and result == true then
+      -- We got a fugitive buffer. Do nothing.
+    else
+      only_has_fugitive_buffer = false
+    end
+  end
+  if only_has_fugitive_buffer then
+    -- Note: quit will not occur if we have unwritten buffers.
+    -- This is intentional!
+    vim.cmd('silent! q')
+  end
 end
 
 vim.api.nvim_create_augroup('FugitiveGitTab', { clear = true })
@@ -24,5 +45,12 @@ vim.api.nvim_create_autocmd(
     -- Required so that fugitive stuff works in this autocmd as expected.
     -- See https://github.com/tpope/vim-fugitive/issues/1554.
     nested = true,
+  }
+)
+vim.api.nvim_create_autocmd(
+  {'TabClosed'},
+  {
+    callback = exit_if_fugitive_window_last,
+    group = 'FugitiveGitTab',
   }
 )
