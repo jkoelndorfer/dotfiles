2c857108b361ef8fbd48b39cedd50fa11c133816
John Koelndorfer <john@johnk.io>
2022-06-28 14:18:37 -0500

    zsh: Prevent asdf script from being multi-sourced

--- zsh/config/rc/80-asdf.zsh
+++ zsh/config/rc/80-asdf.zsh
@@ -1,8 +1,11 @@
+[[ "$ASDF_SOURCED" == '1' ]] && return 0
+
 # If asdf is available, enable it.
 #
 # See https://github.com/asdf-vm/asdf.
 _asdf_lib="$HOME/.asdf/asdf.sh"
 if [[ -f "$_asdf_lib" ]]; then
     source "$_asdf_lib"
+    export ASDF_SOURCED=1
 fi
 unset _asdf_lib
