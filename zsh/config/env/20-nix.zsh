e847c922b7df02f8ee648aaa3132fcf06864f7c7
John Koelndorfer <john@johnk.io>
2022-06-28 14:18:37 -0500

    zsh: Enhance nix profile sourcing
    In addition to preventing multiple-sourcing, nix utilities
    will be added to the _end_ of PATH, rather than to the beginning.

--- zsh/config/env/20-nix.zsh
+++ zsh/config/env/20-nix.zsh
@@ -1,4 +1,25 @@
+[[ "$NIX_SOURCED" == '1' ]] && return 0
+
+pre_nix_path=$PATH
+
 # Primarily for Nix on macOS. Prefer GNU coreutils over BSD coreutils.
 nix_profile='/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 [[ -f "$nix_profile" ]] && source "$nix_profile"
+
+# Nix will put its binaries at the front of our PATH.
+#
+# That's actually not desireable on macOS. Non-system coreutil
+# binaries can cause really weird behavior.
+new_path_len=${#PATH}
+previous_path_len=${#pre_nix_path}
+nix_path=${PATH[0,$(( new_path_len - previous_path_len - 1))]}
+
+export PATH="$pre_nix_path:$nix_path"
+
 unset nix_profile
+unset new_path_len
+unset previous_path_len
+unset pre_nix_path
+unset nix_path
+
+export NIX_SOURCED=1
