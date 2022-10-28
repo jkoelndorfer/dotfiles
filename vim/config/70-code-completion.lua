0e2cacf6f99f2bb682e096e120a43f74f01be06e
John Koelndorfer <john@johnk.io>
2022-10-28 13:22:10 -0500

    vim: Autocomplete path names in buffers

--- vim/config/70-code-completion.lua
+++ vim/config/70-code-completion.lua
@@ -53,8 +53,9 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   sources = cmp.config.sources({
-    { name = 'nvim_lsp', group_index = 1 },
-    { name = 'buffer', group_index = 2 },
+    { name = 'path', group_index = 1 },
+    { name = 'nvim_lsp', group_index = 2 },
+    { name = 'buffer', group_index = 3 },
   })
 })
 
