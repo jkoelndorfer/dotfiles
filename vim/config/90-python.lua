8951ac52f955f9aff6d5169cf5539855b14c6f19
John Koelndorfer <john@johnk.io>
2023-01-15 10:53:02 -0600

    vim: Set pymode max line length = 120

--- vim/config/90-python.lua
+++ vim/config/90-python.lua
@@ -1,2 +1,5 @@
-configure_lang_settings("python", 4, 120, false)
+max_line_length = 120
+configure_lang_settings("python", 4, max_line_length, false)
 configure_lsp("python", "pyright", {vim.env.DOTFILE_DIR .. '/dev/language-servers/python'}, nil)
+
+vim.api.nvim_exec("let g:pymode_options_max_line_length=" .. max_line_length, nil)
