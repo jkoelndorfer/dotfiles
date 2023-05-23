d6fd20e7782f2d65af5fcc0ada8eeeaa8112a54e
John Koelndorfer <john@johnk.io>
2023-05-23 14:53:56 -0500

    vim/telescope: Permit find_files to follow symlinks
    For note-taking, I configure multiple Syncthing directories and only
    sync notes to a device that needs them. There is one main note-taking
    directory with symlinks to work-related notes and personal notes.
    Because I want notes to be integrated as much as they can be, Telescope
    should follow symlinks when operating in the notes directory.

--- vim/config/70-telescope.lua
+++ vim/config/70-telescope.lua
@@ -5,12 +5,16 @@ ts_func = require('telescope.builtin')
 -- us not being in a git repository), falls back to find_files().
 function try_git_files()
   if not pcall(ts_func.git_files) then
-    ts_func.find_files()
+    find_files()
   end
 end
 
+function find_files()
+  ts_func.find_files({ follow = true })
+end
+
 vim.api.nvim_set_keymap('n', '<C-p>', '', { noremap = true, callback = try_git_files })
-vim.api.nvim_set_keymap('n', '<C-M-p>', '', { noremap = true, callback = function() ts_func.find_files() end })
+vim.api.nvim_set_keymap('n', '<C-M-p>', '', { noremap = true, callback = find_files })
 vim.api.nvim_set_keymap('n', '<C-b>', '', { noremap = true, callback = function() ts_func.buffers({ preview = true }) end })
 vim.api.nvim_set_keymap('n', '<C-g>', '', { noremap = true, callback = function() ts_func.live_grep() end })
 vim.api.nvim_set_keymap('n', '<C-f>', '', { noremap = true, callback = function() ts_func.lsp_references({ preview = true }) end })
