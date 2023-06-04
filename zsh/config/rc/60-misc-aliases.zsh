3606b8bf225795ba9b8dbc151f846d64a5515480
John Koelndorfer <john@johnk.io>
2023-06-04 13:28:36 -0500

    zsh: Implement helper for `bw unlock`

--- zsh/config/rc/60-misc-aliases.zsh
+++ zsh/config/rc/60-misc-aliases.zsh
@@ -1,3 +1,16 @@
+function bw() {
+    local bw_output
+    if [[ "$1" == 'unlock' ]]; then
+        bw_output=$(command bw "$@" --raw)
+        rc=$?
+        if [[ -n "$bw_output" ]]; then
+            export BW_SESSION=$bw_output
+        fi
+        return "$rc"
+    fi
+    command bw "$@"
+}
+
 function weather() {
     curl wttr.in/"$1"
 }
