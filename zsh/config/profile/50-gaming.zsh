9f61747404f98a7fdd31b392826ee070652479ba
John Koelndorfer <john@johnk.io>
2023-10-04 17:03:06 -0500

    zsh: Set STAGING_SHARED_MEMORY=1 to enable wine-staging shared memory

--- zsh/config/profile/50-gaming.zsh
+++ zsh/config/profile/50-gaming.zsh
@@ -3,3 +3,7 @@ export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0
 # Store DXVK state cache in a consistent location.
 # See https://github.com/doitsujin/dxvk#state-cache.
 export DXVK_STATE_CACHE_PATH="${HOME}/.cache/dxvk"
+
+# Enable wine-staging's shared memory feature.
+# See https://wiki.winehq.org/Wine-Staging_Environment_Variables.
+export STAGING_SHARED_MEMORY=1
