0fd0a831a9066f77e003b848e2c42fa3d99f5672
John Koelndorfer <john@johnk.io>
2023-10-04 17:02:53 -0500

    zsh: Set DXVK_STATE_CACHE_PATH

--- zsh/config/profile/50-gaming.zsh
+++ zsh/config/profile/50-gaming.zsh
@@ -1,1 +1,5 @@
 export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0
+
+# Store DXVK state cache in a consistent location.
+# See https://github.com/doitsujin/dxvk#state-cache.
+export DXVK_STATE_CACHE_PATH="${HOME}/.cache/dxvk"
