20c82b7153d83d416ba1b6ad837c23017599c70a
John Koelndorfer <john@johnk.io>
2024-01-06 07:50:12 -0600

    vim: Add ruff-lsp for Python files

--- vim/config/90-python.lua
+++ vim/config/90-python.lua
@@ -2,4 +2,10 @@ max_line_length = 120
 configure_lang_settings("python", 4, max_line_length, false)
 configure_lsp("python", "pyright", {vim.env.DOTFILE_DIR .. '/dev/language-servers/python'}, nil)
 
+function ruff_on_attach(client, bufnr)
+  client.server_capabilities.hoverProvider = false
+end
+
+configure_lsp("python", "ruff_lsp", {"ruff-lsp"}, ruff_on_attach)
+
 vim.api.nvim_exec("let g:pymode_options_max_line_length=" .. max_line_length, nil)
