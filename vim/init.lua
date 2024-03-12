ebf2a4a80986846e8e7ed08fd284880b1944e90b
John Koelndorfer <john@johnk.io>
2024-03-12 15:42:55 -0500

    vim: Implement custom config loading

--- vim/init.lua
+++ vim/init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- load custom neovim config
+local nvim_config_dir = vim.fn.stdpath("config") .. "/config"
+
+for _, file_path in pairs(vim.fn.readdir(nvim_config_dir)) do
+	local full_path = nvim_config_dir .. "/" .. file_path
+
+	if string.match(full_path, "%.lua$") then
+		dofile(full_path)
+	elseif string.match(full_path, "%.vim$") then
+		vim.cmd("source " .. full_path)
+	end
+end
