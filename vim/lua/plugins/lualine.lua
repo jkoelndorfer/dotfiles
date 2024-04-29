ab86f98a43f0cbc34b60f4831e1f7274a785b591
John Koelndorfer <john@johnk.io>
2024-04-29 09:58:22 -0500

    vim: Show full file path in status line

--- vim/lua/plugins/lualine.lua
+++ vim/lua/plugins/lualine.lua
@@ -1,3 +1,6 @@
+local lazyvim_config = require("lazyvim.config")
+local icons = lazyvim_config.icons
+
 return {
 	"nvim-lualine/lualine.nvim",
 
@@ -5,6 +8,24 @@ return {
 		options = {
 			section_separators = { left = "", right = " " },
 			component_separators = { left = "", right = " " },
+		},
+
+		sections = {
+			-- Lots of this is copied from
+			-- https://github.com/LazyVim/LazyVim/blob/f086bcde253c29be9a2b9c90b413a516f5d5a3b2/lua/lazyvim/plugins/ui.lua#L141-L154
+			lualine_c = {
+				{
+					"diagnostics",
+					symbols = {
+						error = icons.diagnostics.Error,
+						warn = icons.diagnostics.Warn,
+						info = icons.diagnostics.Info,
+						hint = icons.diagnostics.Hint,
+					},
+				},
+				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+				{ "filename", path = 1 },
+			},
 		},
 	},
 }
