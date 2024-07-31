2bfeefbe80c9697f7dfc21ba2345697594b001c5
John Koelndorfer <john@johnk.io>
2024-07-31 12:29:35 -0500

    vim: Do not replace clipboard contents with deleted text

---
+++ vim/lua/plugins/clipboard.lua
@@ -0,0 +1,5 @@
+-- The LazyVim default is "unnamedplus", which causes text
+-- deletion to replace the contents of your clipboard.
+--
+-- I do not know who thought this was a good idea. It isn't.
+vim.o.clipboard = nil
