8e0df2d911d47d73d689003c5ed813551bd1213c
John Koelndorfer <john@johnk.io>
2025-01-01 13:54:23 -0600

    zsh: Set STEAM_LIBRARY_ROOT appropriately on atomic Linux

--- zsh/config/profile/50-gaming.zsh
+++ zsh/config/profile/50-gaming.zsh
@@ -1,3 +1,5 @@
+source "${DOTFILE_DIR}/zsh/lib/system.zsh"
+
 export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0
 
 # Store DXVK state cache in a consistent location.
@@ -7,3 +9,9 @@ export DXVK_STATE_CACHE_PATH="${HOME}/.cache/dxvk"
 # Enable wine-staging's shared memory feature.
 # See https://wiki.winehq.org/Wine-Staging_Environment_Variables.
 export STAGING_SHARED_MEMORY=1
+
+# If running on an atomic Linux host, set STEAM_LIBRARY_ROOT
+# to the path used by the Steam flatpak.
+if is-atomic-linux-host; then
+  export STEAM_LIBRARY_ROOT="${HOME}/.var/app/com.valvesoftware.Steam/.steam/steam"
+fi
