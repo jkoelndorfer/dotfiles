9145bc042af8213fc06522b386820cea8e4b543b
John Koelndorfer <john@johnk.io>
2025-04-20 08:57:41 -0500

    general/configure-xdg.sh: Skip running on atomic Linux hosts

--- general/configure-xdg.sh
+++ general/configure-xdg.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+source "${DOTFILE_DIR}/shell/lib/system.sh"
+
 if ! type xdg-settings >/dev/null 2>&1; then
+    exit 0
+fi
+
+if in-atomic-linux-context; then
+    # `xdg-settings set` does not work properly on Fedora Kinoite
+    # because qtpaths is not present. Skip setup in that case.
+    printf 'cannot set default browser on atomic Linux host; skipping\n' >&2
     exit 0
 fi
 
