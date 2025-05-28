55501d4fae6bc3ab5e22ff734dc34fa7f4241e79
John Koelndorfer <john@johnk.io>
2025-05-28 08:53:04 -0500

    vim: Highlight trailing whitespace, configure listchars

--- vim/lua/config/options.lua
+++ vim/lua/config/options.lua
@@ -3,3 +3,7 @@
 --
 -- I do not know who thought this was a good idea. It isn't.
 vim.o.clipboard = nil
+
+vim.opt.listchars = "tab: ,trail: "
+-- Highlight trailing whitespace because it's wrong and I hate it.
+vim.cmd([[match ErrorMsg '\s\+$']])
