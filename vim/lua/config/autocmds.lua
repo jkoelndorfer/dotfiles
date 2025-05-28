36e6e9b15fbe3e29dbcdc31f95c5b548aa8a300f
John Koelndorfer <john@johnk.io>
2025-05-28 08:36:44 -0500

    vim: Don't expand tabs in shell scripts

--- vim/lua/config/autocmds.lua
+++ vim/lua/config/autocmds.lua
@@ -4,3 +4,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		vim.wo.conceallevel = 0
 	end,
 })
+
+-- In shell scripts, heredocs can be auto-dedented if tabs
+-- are used instead of spaces. For that reason, prefer
+-- tabs in shell scripts.
+--
+-- https://stackoverflow.com/questions/33815600/indenting-heredocs-with-spaces
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "shell" },
+	callback = function()
+		vim.o.expandtab = false
+	end,
+})
